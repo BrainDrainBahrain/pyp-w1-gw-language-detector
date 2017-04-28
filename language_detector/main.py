c01d5b0f9a7dfe253fe7748425d877d9361b695b
David Bouwkamp <davidbouw@gmail.com>
2017-04-28 01:42:11 +0000

    initial commit

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,44 @@
 # -*- coding: utf-8 -*-
-
+import string 
 """This is the entry point of the program."""
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
+    
+    
     # implement your solution here
-    pass
+    '''
+    LANGUAGES[0...n]['common_words'][0...n]
+    '''
+    #strips puncuation from text
+    txt = text.translate(None, string.punctuation)
+    
+    #generate table of words from 'text'
+    text_table = txt.lower().split()
+    
+    res = []
+    
+    #iterate through languages
+    for i, lang in enumerate(languages):
+        #add space for counting matches to res
+        res.append(0)
+        
+        #iterate through words in active 100 word dictionary
+        for dictword in lang['common_words']:
+            res[i] += text_table.count(dictword)
+            
+    ans = 0
+    ansidx = 0
+    for i, cnt in enumerate(res):
+        if cnt > ans:
+            ans = cnt
+            ansidx = i
+            
+        
+    return languages[ansidx]['name']
+            
+  
+    
+        
+    
+
